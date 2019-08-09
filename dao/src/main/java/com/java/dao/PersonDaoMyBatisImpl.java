b59b0a1da2c419ad4219428a935ee8af427afcad
Dmitriy Bryksin <Dmitriy_Bryksin@epam.com>
2019-08-09 13:28:16 +0300

    add try-with-resources in DaoImp

--- dao/src/main/java/com/java/dao/PersonDaoMyBatisImpl.java
+++ dao/src/main/java/com/java/dao/PersonDaoMyBatisImpl.java
@@ -4,6 +4,7 @@ import com.java.mappers.PersonMapper;
 import com.java.model.Person;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,42 +18,41 @@ public class PersonDaoMyBatisImpl implements PersonDao {
 
     public  Person findById(String id){
         UUID uuid = UUID.fromString(id);
-        SqlSession session = sessionManager.getSession();
-        Person person = session.getMapper(PersonMapper.class).getPersonById(uuid);
-        session.close();
+        Person person;
+        try(SqlSession session = sessionManager.getSession()){
+             person = session.getMapper(PersonMapper.class).getPersonById(uuid);
+        }
         return person;
     }
 
     public  List<Person> findAll(){
-        SqlSession session = sessionManager.getSession();
-        List<Person> personList =  session.getMapper(PersonMapper.class).getPersonList();
-        session.close();
+        List<Person> personList = new ArrayList<>();
+        try(SqlSession session = sessionManager.getSession()) {
+            personList = session.getMapper(PersonMapper.class).getPersonList();
+        }
         return personList;
     }
 
     public void save(Person person){
-        UUID uuid = UUID.randomUUID();
-        person.setId(uuid);
-        SqlSession session = sessionManager.getSession();
-        session.getMapper(PersonMapper.class).insertPerson(person);
-        session.commit();
-        session.close();
+        person.setId(UUID.randomUUID());
+        try( SqlSession session = sessionManager.getSession()){
+            session.getMapper(PersonMapper.class).insertPerson(person);
+            session.commit();
+        }
     }
 
     public void deleteById(String id){
-        UUID uuid = UUID.fromString(id);
-        SqlSession session = sessionManager.getSession();
-        session.getMapper(PersonMapper.class).deletePersonById(uuid);
-        session.commit();
-        session.close();
+        try(SqlSession session = sessionManager.getSession()) {
+            session.getMapper(PersonMapper.class).deletePersonById(UUID.fromString(id));
+            session.commit();
+        }
     }
 
     public void update(Person person, String id){
-        UUID uuid = UUID.fromString(id);
-        SqlSession session = sessionManager.getSession();
-        person.setId(uuid);
-        session.getMapper(PersonMapper.class).updatePerson(person);
-        session.commit();
-        session.close();
+        try(SqlSession session = sessionManager.getSession()) {
+            person.setId(UUID.fromString(id));
+            session.getMapper(PersonMapper.class).updatePerson(person);
+            session.commit();
+        }
     }
 }
